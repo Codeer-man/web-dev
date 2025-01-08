91ca9cf76572da66da649f70ba2bcd10d84211ac
Codeer-man <mdrmoney34@gmail.com>
2025-01-08 19:43:12 +0545

    Delete button and its function

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,38 +1,27 @@
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Aboutus from "./pages/Aboutus";
+import AboutUs from "./pages/Aboutus"; // Renamed for consistency
 import Services from "./pages/Services";
-import Contact from "./pages/contactus";
-import LogIn from "./pages/login";
-import Error from "./pages/Error";
-import UseCase from "./pages/UseCase";
-import DataProvider from "./pages/DatProvider";
-import { Children, useContext } from "react";
-function App() {
+import Contact from "./pages/contactus"; // Renamed for consistency
+import Login from "./pages/Login";
+import Usecase from "./pages/Usecase";
 
-  const data = useContext(DataProvider)
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} errorElement={<Error />} />
-        <Route path="/aboutus" element={<Aboutus />} />
-        <Route path="/service" element={<Services />} />
+        {/* Define routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/UseCase" element={<UseCase />} />
-        <Route path="/DataProvider" element={<DataProvider />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/usecase" element={<Usecase />} />
       </Routes>
       <Footer />
-      <DataProvider>
-        {Children}
-      </DataProvider>
     </Router>
   );
 }
--- react/my-app/src/pages/Home.jsx
+++ react/my-app/src/pages/Home.jsx
@@ -1,12 +1,7 @@
-import { Child } from "./contactus"
+import React from "react";
 
-export default function Home() {
+const Home = () => {
+  return <div>home</div>;
+};
 
-  const message = "hello pokhara"
-  return (
-    <div className="h-screen w-screen"  >
-      <h1>This is Home page</h1>
-      <Child ContactMessage={message}/>
-    </div>
-  )
-}
+export default Home;
