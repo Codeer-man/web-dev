9d182dc5555a9392d6666a0430a40e6c286e1fc1
Codeer-man <mdrmoney34@gmail.com>
2025-01-29 15:01:26 +0545

    Adding error handling

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@
 import express from 'express';
 import router from './router/auth-router.js';
 import connectDb from './utils/db.js';
+import errorMiddleware from './middleware/error-middleware.js';
+import contactRouter from "./router/contact-router.js"
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth", router);
+app.use("/api/form", contactRouter);
+app.use(errorMiddleware)
 
 const PORT = 5000;
 
