c225472b62fa7d7fbdcde1fe7088921b6a6f012e
Codeer-man <mdrmoney34@gmail.com>
2025-03-17 21:21:27 +0545

    type: module

--- node/express/app.js
+++ node/express/app.js
@@ -1,12 +1,14 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
 
 const app = express();
+dotenv.config();
 
-const staticPath = path.join(__dirname, "public");
-app.use(express.static(staticPath));
+const staticpath = path.join(import.meta.dirname, "public");
+// const staticPath = path.join(__dirname, "public");
+app.use("/public", express.static(staticpath));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
